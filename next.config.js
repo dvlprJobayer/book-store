170add854ccc5971ad3b7e2e870d735a66ea6462
dvlprjobayer <nesarahammed019@gmail.com>
2023-06-10 18:41:35 +0600

    initial commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
