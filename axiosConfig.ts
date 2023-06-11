d229cba21ed0b8488aa49b96e93bbb1622783362
dvlprjobayer <nesarahammed019@gmail.com>
2023-06-12 01:23:44 +0600

    axios config changed

---
+++ axiosConfig.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosError, AxiosHeaders } from "axios";
+import Cookies from "js-cookie";
+
+const instance = axios.create({
+  baseURL: "https://book-store-js-backend.onrender.com/api/v1",
+});
+
+instance.defaults.withCredentials = true;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    const headers: AxiosHeaders = config.headers as AxiosHeaders;
+
+    if (Cookies.get("jwtoken") && !headers.has("Authorization")) {
+      headers.set("Authorization", `Bearer ${Cookies.get("jwtoken")}`);
+    }
+    config.headers = headers;
+    return config;
+  },
+  function (error: AxiosError) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(error);
+    if (error?.response?.status === 401 || error.status === 401) {
+      Cookies.remove("jwtoken");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
